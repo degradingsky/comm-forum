4c2a76713c9d9deca298dfb09dec47135e87c065
degradingsky <anand.rishu07@gmail.com>
2025-04-17 23:43:53 +0530

    auth0 authentication added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,63 +1,30 @@
-'use client';
+import { auth0 } from "@/lib/auth0"
+import { redirect } from 'next/navigation';
+import HomeClient from './components/HomeClient';
+import api from "@/lib/axios";
 
-import { useRouter } from 'next/navigation';
-import { AppBar, Toolbar, Typography, Button, Box, Card, CardContent } from '@mui/material';
+export default async function HomePage() {
+  const session = await auth0.getSession();
 
-export default function Home() {
-  const router = useRouter();
+  if (!session?.user) {
+    redirect('/auth/login');
+  }
 
-  const forumList = [
-    { id: '1', title: 'forum1', desc: 'this is forum1', name: 'Rishu', createdAt: '15 Apr, 2025' },
-    { id: '2', title: 'forum2', desc: 'this is forum2', name: 'User2', createdAt: '15 Apr, 2025' },
-    { id: '3', title: 'forum3', desc: 'this is forum3', name: 'User3', createdAt: '15 Apr, 2025' },
-  ];
+  let forumList = [];
+  console.log('====>>>', session)
 
-  const handleCardClick = (id: string) => {
-    router.push(`/discussion/${id}`);
-  };
+  const token = session.tokenSet.accessToken
+  
+  try {
+    const res = await api.get('/forums', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    forumList = res.data;
+  } catch (error) {
+    console.error('Error fetching forums:', error);
+  }
 
-  const handleLogout = () => {
-    console.log('Logged out');
-  };
-
-  return (
-    <>
-      <AppBar position="static">
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Typography variant="h6" component="div">
-            Forum
-          </Typography>
-          <Button color="inherit" onClick={handleLogout}>
-            Logout
-          </Button>
-        </Toolbar>
-      </AppBar>
-
-      <Box sx={{ padding: 4 }}>
-        <Typography variant="h4">
-          Forum Discussions
-        </Typography>
-
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, marginTop: 4 }}>
-          {forumList.map((item) => (
-            <Card
-              key={item.id}
-              variant="outlined"
-              sx={{
-                cursor: 'pointer',
-                transition: '0.2s',
-                '&:hover': { boxShadow: 4 },
-              }}
-              onClick={() => handleCardClick(item.id)}
-            >
-              <CardContent>
-                <Typography variant="h6">{item.title}</Typography>
-                <Typography>{`${item?.name} posted on ${item.createdAt}`}</Typography>
-              </CardContent>
-            </Card>
-          ))}
-        </Box>
-      </Box>
-    </>
-  );
+  return <HomeClient user={session.user} forumList={forumList} />;
 }
